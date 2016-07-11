cfillion/metaheader

commit 0ef673a6137c0903093ec2385d3be2edeff47778

more robust implementation of previous commit

test/test_parser.rb
@@ -184,14 +184,17 @@ class TestParser < MiniTest::Test
 
   def test_multiline_empty_line
     mh = MetaHeader.new <<-IN
-    -- @test
-    --   Hello
+    --@test
+    --  Hello
     --
-    --   World
+    --  World
     --
+    --@chunky
+    --  bacon
     IN
 
     assert_equal "Hello\n\nWorld", mh[:test]
+    assert_equal 'bacon', mh[:chunky] # no leading newline
   end
 
   def test_read_file

lib/metaheader.rb
@@ -64,8 +64,6 @@ class MetaHeader
     @data = {}
 
     @last_key = nil
-    @last_prefix = String.new
-    @line_breaks = 0
 
     input = input.encode universal_newline: true
     input.each_line {|line|
@@ -227,29 +225,29 @@ private
 
     @last_key = key.to_sym
     @data[@last_key] = Tag.new match[:key].freeze, value
+    @line_breaks = 0
   end
 
   def append(line)
-    # remove the line prefix
-    mline = line[@last_prefix.size..-1]
-
-    if mline&.empty?
+    if line.rstrip == @last_prefix.rstrip
       @line_breaks += 1
       return true # add the line break later
-    elsif !line.start_with? @last_prefix
-      return
-    else
+    elsif line.start_with? @last_prefix
+      mline = line[@last_prefix.size..-1]
       stripped = mline.strip
       indent_level = mline.index stripped
       return if indent_level < 1
+    else
+      return
     end
 
     tag = @data[@last_key]
-
     tag.value = @raw_value.to_s unless tag.value.is_a? String
+
     @line_breaks += 1 unless tag.value.empty?
     tag.value += "\n" * @line_breaks
     @line_breaks = 0
+
     tag.value += stripped
     stripped
   end

commit f0c8644bb42dd8ebe5c0dcea7f9f1344cb9e9500

add a test to prevent possible regression from 0ef673a6137c0903093ec2385d3be2edeff47778
if we don't check the start of the line against the STRIPPED prefix

test/test_parser.rb
@@ -197,6 +197,17 @@ class TestParser < MiniTest::Test
     assert_equal 'bacon', mh[:chunky] # no leading newline
   end
 
+  def test_multiline_empty_line_space_prefix
+    mh = MetaHeader.new <<-IN
+    -- @test
+    --   Hello
+    --
+    --   World
+    IN
+
+    assert_equal "Hello\n\nWorld", mh[:test]
+  end
+
   def test_read_file
     path = File.expand_path '../input/basic_tag', __FILE__
     mh = MetaHeader.from_file path